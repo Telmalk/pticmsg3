94c5efaed9cace537f4b69c4f58a50fb25326657
Clément Haller <telmalk95@gmail.com>
2020-04-27 13:47:23 +0200

    step1

---
+++ includes/data.php
@@ -0,0 +1,12 @@
+<?php
+    $data = [
+      "got" => [
+          "title" => "Game of thrones",
+          "description" => "Neuf familles nobles rivalisent pour le contrôle du Trône de Fer dans les sept royaumes de Westeros. Pendant ce temps, des anciennes créatures mythiques oubliées reviennent pour faire des ravages.",
+          "date" => "27/04/20",
+          "img" => "got.jpg",
+          "img-alt" => "Représentation du trone de fer",
+          "span-text" => "palpitant",
+          "span-label" => "success",
+      ]
+    ];
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+    define("APP_ROOT_DIR", dirname(__DIR__) . "/");
+
+    require_once APP_ROOT_DIR . "includes/function.php";
+    require_once APP_ROOT_DIR . "includes/data.php";
+
+    $dataPage = $data['got'];
+    getHeader();
+    getPage($dataPage);
+    getFooter();
